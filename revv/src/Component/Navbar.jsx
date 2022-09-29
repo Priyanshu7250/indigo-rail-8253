badc951b32bc266b1368ebe3b9a29a6be1816090
Priyanshu7250 <kpriyanshu06@gmail.com>
2022-09-29 13:17:28 +0530

    abbb

--- revv/src/Component/Navbar.jsx
+++ revv/src/Component/Navbar.jsx
@@ -1,8 +1,44 @@
+
+
 import React from "react";
 function Navbar(){
     return(
         <>
-          <h1>Hello React</h1>
+          <nav className="main-nav">
+            {/* first logo div */}
+            <div className="logo">
+                <img src="https://www.revv.co.in/grapheneImages/newopen/logo.svg" alt="" />
+                <h4>santisied. Safe</h4>
+            </div>
+
+            <div className="nav-link">
+                  <ul>
+                    <li>
+                      <a href="#">
+                        
+                          <span>Hyundai</span>Subcription
+                          
+                        
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#">
+                      <span>Mahindra</span>Subcription
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="#">FAQs</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Login or Signup</a>
+                    </li>
+                  </ul>
+            </div>
+
+          </nav>
         </>
     )
 }
