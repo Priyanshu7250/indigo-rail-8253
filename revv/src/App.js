6bbc71cb8b1140706b1d4be0f3ce917e2dbd0f4a
Priyanshu7250 <kpriyanshu06@gmail.com>
2022-09-29 23:00:34 +0530

    anjk

--- revv/src/App.js
+++ revv/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Navbar from"./Component/Navbar";
+import Middle from "./Component/Middle";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Middle/>
     </div>
   );
 }
---
+++ revv/src/Component/Middle.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function Middle(){
+    return(
+      <>
+      <div className="top-section">
+         <div className="left-image">
+           <img src="https://www.revv.co.in/imgs/logo-rentals.svg" alt="" />
+         </div>
+         <div className="right-image">
+           <img src="https://www.revv.co.in/assets/RentalImages/HomeScreen/heroCarousel/ST_Desktop_1_FG.webp" alt="" />
+         </div>
+      </div>
+      
+      </>
+    )
+};
+export default Middle;
